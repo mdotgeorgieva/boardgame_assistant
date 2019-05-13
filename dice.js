9df8e83e0f0776ac052390b4aefde54ffc64d151
Maria Mateeva-Georgieva <m.mateeva90@gmail.com>
2019-05-13 12:02:41 +0300

    Dice generator now shows images of results/ no formatting

--- dice.js
+++ dice.js
@@ -2,8 +2,9 @@
 
 let savedType;
 let savedNumber;
-let rollDice = document.getElementById('choose');
-let results = [];
+const rollDice = document.getElementById('choose');
+let diceResults = [];
+const diceDisplayResults = document.getElementById('diceResults');
 
 //Save number of dice
 
@@ -25,15 +26,57 @@ let chooseType = function() {
 
 let randomizer = function() {
 		let randomNumber = Math.floor(Math.random() * savedType + 1);
-		results.push(randomNumber);
-		console.log(results);
-		return results;
+		diceResults.push(randomNumber);
+		console.log(diceResults);
+		return diceResults;
 	}
 
 
 //Display results
 
+let diceDivs = (x) => {
+	let diceDiv = document.createElement('div');
+	diceDiv.setAttribute('id', 'diceResult' + x);
+	diceDiv.setAttribute('class', 'diceResult');
+	diceDisplayResults.appendChild(diceDiv);
+}
 
+let dicePrintResults = (x) =>{
+	let dicePlayerResult = document.createElement('img');
+	dicePlayerResult.setAttribute('class', 'diceImage')
+	let y = document.getElementById('diceResult' + x)
+	switch(diceResults[x]){
+		case 1:
+		dicePlayerResult.setAttribute('src', 'images/dice/one.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+
+		case 2:
+		dicePlayerResult.setAttribute('src', 'images/dice/two.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+
+		case 3:
+		dicePlayerResult.setAttribute('src', 'images/dice/three.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+
+		case 4:
+		dicePlayerResult.setAttribute('src', 'images/dice/four.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+
+		case 5:
+		dicePlayerResult.setAttribute('src', 'images/dice/five.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+
+		case 6:
+		dicePlayerResult.setAttribute('src', 'images/dice/six.svg');
+		y.appendChild(dicePlayerResult);
+		break;
+	}
+}
 
 //Onclick
 
@@ -42,6 +85,8 @@ rollDice.onclick = function() {
 	chooseType();
 	for(let i = 0; i < savedNumber; i++){
 		randomizer();
+		diceDivs(i);
+		dicePrintResults(i);
 	}
-	results = [];
+	diceResults = [];
 } 

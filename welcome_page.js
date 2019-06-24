e4bf91bf20b2f7d3e14ca6d385279f0b14edef79
Maria Mateeva-Georgieva <m.mateeva90@gmail.com>
2019-06-24 12:01:16 +0300

    Bug fixed (dice-rps toggle not working)

--- welcome_page.js
+++ welcome_page.js
@@ -9,7 +9,7 @@ const rpsSection = document.getElementById("rpsSection");
 //Toggle "hidden"
 diceImage.onclick = () => {
     if(rpsSection.classList.contains("hidden") == false){
-        diceSection.classList.toggle("hidden");
+        rpsSection.classList.toggle("hidden");
     }
     diceSection.classList.toggle("hidden");
 }
